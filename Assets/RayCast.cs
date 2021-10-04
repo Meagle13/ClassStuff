3543a2da7fbb46ecec7c0bd928ddcfbb77491246
Michael <michaeldavy347@outlook.com>
2021-10-04 15:10:19 +1300

    Project Intitalized + Raycast
    Setup project
    Added Raycast test

---
+++ Assets/RayCast.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayCast : MonoBehaviour
+{
+    public LayerMask mask;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.Mouse0))
+        {
+            RaycastHit hit;
+            Debug.DrawRay(Vector3.zero, transform.forward * 40, Color.magenta);
+            if(Physics.Raycast(Vector3.zero, transform.forward, out hit, 40, mask))
+            {
+                Debug.Log(hit.transform.name);
+                Destroy(hit.transform.gameObject);
+            }
+
+        }
+    }
+}
